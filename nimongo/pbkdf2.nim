762c1847501dedcb304796a2377d25dad7fbf3c1
Rostyslav Dzinko <rostislav.dzinko@gmail.com>
2015-11-06 19:46:42 +0200

    Fixed bad argument names

--- nimongo/pbkdf2.nim
+++ nimongo/pbkdf2.nim
@@ -3,4 +3,4 @@
 
 proc PKCS5_PBKDF2_HMAC_SHA1*(password: pointer, passlen: uint, salt: pointer, saltlen: uint, iterations: uint, keylen: uint, k: pointer): cint {.importc, header: "<openssl/evp.h>", nodecl.}
 proc EVP_sha1*(): pointer {.importc, header: "<openssl/evp.h>".}
-proc HMAC*(hmac: pointer, key: pointer, keylen: cint, data: pointer, datalen: cint, xuinia1: pointer=nil, xuinia2: pointer=nil): cstring {.importc, header:"<openssl/hmac.h>", nodecl.}
+proc HMAC*(hmac: pointer, key: pointer, keylen: cint, data: pointer, datalen: cint, x1: pointer=nil, x2: pointer=nil): cstring {.importc, header:"<openssl/hmac.h>", nodecl.}
