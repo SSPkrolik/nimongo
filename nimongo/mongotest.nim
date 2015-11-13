b15794554db2c34d3e3386952020c3a752bd61e0
Rostyslav Dzinko <rostislav.dzinko@gmail.com>
2015-11-13 16:14:55 +0200

    Used  operator for creating BSON documents in mongo test suite

--- nimongo/mongotest.nim
+++ nimongo/mongotest.nim
@@ -46,10 +46,10 @@ suite "Mongo instance administration commands test suite":
     check(waitFor(adb.drop()) == true)
 
   test "[ASYNC] [SYNC] Command: 'listDatabases'":
-    sco.insert(B("test", "test"))
+    sco.insert(%*{"test": "test"})
     check("testdb" in sm.listDatabases())
     check("testdb" in waitFor(am.listDatabases()))
-    sco.remove(B("test", "test"), RemoveSingle)
+    sco.remove(%*{"test": "test"}, RemoveSingle)
 
   test "[     ] [SYNC] Command: 'listCollections'":
     let sclist = sdb.listCollections()
@@ -77,7 +77,7 @@ suite "Authentication":
     check(sdb.createUser("test", "test"))
     let authtestdb = newMongoDatabase("mongodb://test:test@localhost:27017/testdb")
     check($authtestdb == "testdb")
-    authtestdb[TestSyncCol].insert(B("data", "auth"))
+    authtestdb[TestSyncCol].insert(%*{"data": "auth"})
     check(sdb.dropUser("test"))
 
 suite "User Management":
@@ -111,34 +111,34 @@ suite "Mongo collection-level operations":
 
     check(sco.insert(
       @[
-        B("iter", 0.int32)("label", "l"),
-        B("iter", 1.int32)("label", "l"),
-        B("iter", 2.int32)("label", "l"),
-        B("iter", 3.int32)("label", "l"),
-        B("iter", 4.int32)("label", "l")
+        %*{"iter": 0.int32, "label": "l"},
+        %*{"iter": 1.int32, "label": "l"},
+        %*{"iter": 2.int32, "label": "l"},
+        %*{"iter": 3.int32, "label": "l"},
+        %*{"iter": 4.int32, "label": "l"},
       ]
     ))
     check(sco.count() == 5)
 
     check(waitFor(aco.insert(
       @[
-        B("iter", 0.int32)("label", "l"),
-        B("iter", 1.int32)("label", "l"),
-        B("iter", 2.int32)("label", "l"),
-        B("iter", 3.int32)("label", "l"),
-        B("iter", 4.int32)("label", "l")
+        %*{"iter": 0.int32, "label": "l"},
+        %*{"iter": 1.int32, "label": "l"},
+        %*{"iter": 2.int32, "label": "l"},
+        %*{"iter": 3.int32, "label": "l"},
+        %*{"iter": 4.int32, "label": "l"},
       ]
     )))
     check(waitFor(aco.count()) == 5)
 
   test "[ASYNC] [SYNC] 'drop' collection":
-    check(sco.insert(B("svalue", "hello")) == true)
+    check(sco.insert(%*{"svalue": "hello"}) == true)
     discard sco.drop()
-    check(sco.find(B("svalue", "hello")).all().len() == 0)
+    check(sco.find(%*{"svalue": "hello"}).all().len() == 0)
 
-    check(waitFor(aco.insert(B("svalue", "hello"))) == true)
+    check(waitFor(aco.insert(%*{"svalue": "hello"})) == true)
     discard waitFor(aco.drop())
-    check(waitFor(aco.find(B("svalue", "hello")).all()).len() == 0)
+    check(waitFor(aco.find(%*{"svalue": "hello"}).all()).len() == 0)
 
 
 suite "Mongo client operations test suite":
@@ -162,73 +162,73 @@ suite "Mongo client operations test suite":
     check($aco == "testdb.async")
 
   test "[ASYNC] [SYNC] Inserting single document":
-    check(sco.insert(B("double", 3.1415)) == true)
-    check(waitFor(aco.insert(B("double", 3.1415))) == true)
+    check(sco.insert(%*{"double": 3.1415}) == true)
+    check(waitFor(aco.insert(%*{"double": 3.1415})) == true)
 
   test "[ASYNC] [SYNC] Inserting multiple documents":
     let
-      doc1 = B("integer", 100'i32)
-      doc2 = B("string", "hello")("subdoc", B("name", "John"))
-      doc3 = B("array", @["element1", "element2", "element3"])
+      doc1 = %*{"integer": 100'i32}
+      doc2 = %*{"string": "hello", "subdoc": {"name": "John"}}
+      doc3 = %*{"array": ["element1", "element2", "element3"]}
 
     check(sco.insert(@[doc1, doc2, doc3]) == true)
     check(waitFor(aco.insert(@[doc1, doc2, doc3])) == true)
 
   test "[ASYNC] [SYNC] Update single document":
     let
-      selector = B("integer", "integer")
-      updater  = B("$set", B("integer", "string"))
+      selector = %*{"integer": "integer"}
+      updater  = %*{"$set": {"integer": "string"}}
 
     check(sco.insert(@[selector, selector]) == true)
-    #check(waitFor(aco.insert(@[selector, selector])) == true)
+    check(waitFor(aco.insert(@[selector, selector])) == true)
 
     check(sco.update(selector, updater, UpdateSingle, NoUpsert) == true)
-    #check(waitFor(aco.update(selector, updater, UpdateSingle, NoUpsert)) == true)
+    check(waitFor(aco.update(selector, updater, UpdateSingle, NoUpsert)) == true)
 
-    check(sco.find(B("integer", "string")).all().len() == 1)
-    #check(waitFor(aco.find(B("integer", "string")).all()).len() == 1)
+    check(sco.find(%*{"integer": "string"}).all().len() == 1)
+    check(waitFor(aco.find(%*{"integer": "string"}).all()).len() == 1)
 
   test "[ASYNC] [SYNC] Update multiple documents":
     let
-      selector = B("integer", 100'i32)
-      updater  = B("$set", B("integer", 200'i32))
+      selector = %*{"integer": 100'i32}
+      updater  = %*{"$set": {"integer": 200'i32}}
 
     check(sco.insert(@[selector, selector]) == true)
-    #check(waitFor(aco.insert(@[selector, selector])) == true)
+    check(waitFor(aco.insert(@[selector, selector])) == true)
 
     check(sco.update(selector, updater, UpdateMultiple, NoUpsert) == true)
-    #check(waitFor(aco.update(selector, updater, UpdateMultiple, NoUpsert)) == true)
+    check(waitFor(aco.update(selector, updater, UpdateMultiple, NoUpsert)) == true)
 
-    check(sco.find(B("integer", 200'i32)).all().len() == 2)
-    #check(waitFor(aco.find(B("integer", 200'i32)).all()).len() == 2)
+    check(sco.find(%*{"integer": 200'i32}).all().len() == 2)
+    check(waitFor(aco.find(%*{"integer": 200'i32}).all()).len() == 2)
 
   test "[ASYNC] [SYNC] Upsert":
     let
-      selector = B("integer", 100'i64)
-      updater  = B("$set", B("integer", 200'i64))
+      selector = %*{"integer": 100'i64}
+      updater  = %*{"$set": {"integer": 200'i64}}
 
     check(sco.update(selector, updater, UpdateSingle, Upsert) == true)
     check(waitFor(aco.update(selector, updater, UpdateSingle, Upsert)) == true)
 
-    check(sco.find(B("integer", 200)).all().len() == 1)
-    check(waitFor(aco.find(B("integer", 200)).all()).len() == 1)
+    check(sco.find(%*{"integer": 200}).all().len() == 1)
+    check(waitFor(aco.find(%*{"integer": 200}).all()).len() == 1)
 
   test "[ASYNC] [SYNC] Remove single document":
-    let doc = B("string", "hello")
+    let doc = %*{"string": "hello"}
     check(sco.insert(doc) == true)
     check(sco.remove(doc, RemoveSingle) == true)
     check(waitFor(aco.insert(doc)) == true)
     check(waitFor(aco.remove(doc, RemoveSingle)) == true)
 
   test "[ASYNC] [SYNC] Remove multiple documents":
-    check(sco.insert(@[B("string", "value"), B("string", "value")]) == true)
-    check(sco.remove(B("string", "value"), RemoveMultiple))
-    check(sco.find(B("string", "value")).all().len() == 0)
+    check(sco.insert(@[%*{"string": "value"}, %*{"string": "value"}]) == true)
+    check(sco.remove(%*{"string": "value"}, RemoveMultiple))
+    check(sco.find(%*{"string": "value"}).all().len() == 0)
 
-    check(waitFor(aco.insert(@[B("string", "value"), B("string", "value")])) == true)
+    check(waitFor(aco.insert(@[%*{"string": "value"}, %*{"string": "value"}])) == true)
     waitFor(sleepAsync(1))
-    check(waitFor(aco.remove(B("string", "value"), RemoveMultiple)) == true)
-    check(waitFor(aco.find(B("string", "value")).all()).len() == 0)
+    check(waitFor(aco.remove(%*{"string": "value"}, RemoveMultiple)) == true)
+    check(waitFor(aco.find(%*{"string": "value"}).all()).len() == 0)
 
 suite "Mongo aggregation commands":
 
@@ -239,18 +239,18 @@ suite "Mongo aggregation commands":
     discard waitFor(aco.drop())
 
   test "[ASYNC] [SYNC] Count documents in query result":
-    sco.insert(@[B("string", "value"), B("string", "value")])
-    check(sco.find(B("string", "value")).count() == 2)
+    sco.insert(@[%*{"string": "value"}, %*{"string": "value"}])
+    check(sco.find(%*{"string": "value"}).count() == 2)
 
-    check(waitFor(aco.insert(@[B("string", "value"), B("string", "value")])) == true)
-    check(waitFor(aco.find(B("string", "value")).count()) == 2)
+    check(waitFor(aco.insert(@[%*{"string": "value"}, %*{"string": "value"}])) == true)
+    check(waitFor(aco.find(%*{"string": "value"}).count()) == 2)
 
   test "[ASYNC] [SYNC] Query distinct values by field in collection documents":
-    sco.insert(@[B("string", "value")("int", 1'i64), B("string", "value")("double", 2.0)])
-    check(sco.find(B("string", "value")).unique("string") == @["value"])
+    sco.insert(@[%*{"string": "value", "int": 1'i64}, %*{"string": "value", "double": 2.0}])
+    check(sco.find(%*{"string": "value"}).unique("string") == @["value"])
 
-    check(waitFor(aco.insert(@[B("string", "value")("int", 1'i64), B("string", "value")("double", 2.0)])) == true)
-    check(waitFor(aco.find(B("string", "value")).unique("string")) == @["value"])
+    check(waitFor(aco.insert(@[%*{"string": "value", "int": 1'i64}, %*{"string": "value", "double": 2.0}])) == true)
+    check(waitFor(aco.find(%*{"string": "value"}).unique("string")) == @["value"])
 
 suite "Mongo client querying test suite":
 
@@ -262,70 +262,70 @@ suite "Mongo client querying test suite":
 
   test "[ASYNC] [SYNC] Query single document":
     let myId = genOid()
-    check(sco.insert(B("string", "somedoc")("myid", myId)))
-    check(sco.find(B("myid", myId)).one()["myid"] == myId)
+    check(sco.insert(%*{"string": "somedoc", "myid": myId}))
+    check(sco.find(%*{"myid": myId}).one()["myid"] == myId)
 
-    check(waitFor(aco.insert(B("string", "somedoc")("myid", myId))) == true)
-    check(waitFor(aco.find(B("myid", myId)).one())["myid"] == myId)
+    check(waitFor(aco.insert(%*{"string": "somedoc", "myid": myId})) == true)
+    check(waitFor(aco.find(%*{"myid": myId}).one())["myid"] == myId)
 
   test "[ASYNC] [SYNC] Query multiple documents as a sequence":
-    check(sco.insert(@[B("string", "value"), B("string", "value")]) == true)
-    check(sco.find(B("string", "value")).all().len() == 2)
+    check(sco.insert(@[%*{"string": "value"}, %*{"string": "value"}]) == true)
+    check(sco.find(%*{"string": "value"}).all().len() == 2)
 
-    check(waitFor(aco.insert(@[B("string", "value"), B("string", "value")])) == true)
-    check(waitFor(aco.find(B("string", "value")).all()).len() == 2)
+    check(waitFor(aco.insert(@[%*{"string": "value"}, %*{"string": "value"}])) == true)
+    check(waitFor(aco.find(%*{"string": "value"}).all()).len() == 2)
 
   test "[ N/A ] [SYNC] Query multiple documents as iterator":
-    check(sco.insert(B("string", "hello")))
-    check(sco.insert(B("string", "hello")))
-    for document in sco.find(B("string", "hello")).items():
+    check(sco.insert(%*{"string": "hello"}))
+    check(sco.insert(%*{"string": "hello"}))
+    for document in sco.find(%*{"string": "hello"}).items():
       check(document["string"] == "hello")
 
   test "[ASYNC] [SYNC] Query multiple documents up to limit":
     check(sco.insert(
       @[
-        B("iter", 0.int32)("label", "l"),
-        B("iter", 1.int32)("label", "l"),
-        B("iter", 2.int32)("label", "l"),
-        B("iter", 3.int32)("label", "l"),
-        B("iter", 4.int32)("label", "l")
+        %*{"iter": 0.int32, "label": "l"},
+        %*{"iter": 1.int32, "label": "l"},
+        %*{"iter": 2.int32, "label": "l"},
+        %*{"iter": 3.int32, "label": "l"},
+        %*{"iter": 4.int32, "label": "l"}
       ]
     ))
-    check(sco.find(B("label", "l")).limit(3).all().len() == 3)
+    check(sco.find(%*{"label": "l"}).limit(3).all().len() == 3)
 
     check(waitFor(aco.insert(
       @[
-        B("iter", 0.int32)("label", "l"),
-        B("iter", 1.int32)("label", "l"),
-        B("iter", 2.int32)("label", "l"),
-        B("iter", 3.int32)("label", "l"),
-        B("iter", 4.int32)("label", "l"),
+        %*{"iter": 0.int32, "label": "l"},
+        %*{"iter": 1.int32, "label": "l"},
+        %*{"iter": 2.int32, "label": "l"},
+        %*{"iter": 3.int32, "label": "l"},
+        %*{"iter": 4.int32, "label": "l"}
       ]
     )))
-    check(waitFor(aco.find(B("label", "l")).limit(3).all()).len() == 3)
+    check(waitFor(aco.find(%*{"label": "l"}).limit(3).all()).len() == 3)
 
   test "[ASYNC] [SYNC] Skip documents":
     check(sco.insert(
       @[
-        B("iter", 0.int32)("label", "l"),
-        B("iter", 1.int32)("label", "l"),
-        B("iter", 2.int32)("label", "l"),
-        B("iter", 3.int32)("label", "l"),
-        B("iter", 4.int32)("label", "l")
+        %*{"iter": 0.int32, "label": "l"},
+        %*{"iter": 1.int32, "label": "l"},
+        %*{"iter": 2.int32, "label": "l"},
+        %*{"iter": 3.int32, "label": "l"},
+        %*{"iter": 4.int32, "label": "l"},
       ]
     ))
-    check(sco.find(B("label", "l")).skip(3).all().len() == 2)
+    check(sco.find(%*{"label": "l"}).skip(3).all().len() == 2)
 
     check(waitFor(aco.insert(
       @[
-        B("iter", 0.int32)("label", "l"),
-        B("iter", 1.int32)("label", "l"),
-        B("iter", 2.int32)("label", "l"),
-        B("iter", 3.int32)("label", "l"),
-        B("iter", 4.int32)("label", "l")
+        %*{"iter": 0.int32, "label": "l"},
+        %*{"iter": 1.int32, "label": "l"},
+        %*{"iter": 2.int32, "label": "l"},
+        %*{"iter": 3.int32, "label": "l"},
+        %*{"iter": 4.int32, "label": "l"},
       ]
     )))
-    check(waitFor(aco.find(B("label", "l")).skip(3).all()).len() == 2)
+    check(waitFor(aco.find(%*{"label": "l"}).skip(3).all()).len() == 2)
 
 echo ""
 
