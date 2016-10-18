be2976e10fb2fa5d0f2c990f502e75610b5806f7
goldenreign <regfile@yandex.ru>
2016-10-18 18:22:17 +0700

    Database reply parsing unified

--- nimongo/reply.nim
+++ nimongo/reply.nim
@@ -1,7 +1,72 @@
+import bson
+import errors
+
 type StatusReply* = object  ## Database Reply
     ok*: bool
     n*: int
     err*: string
+
+template parseReplyField(b: untyped, field: untyped, default: untyped, body: untyped): untyped =
+  ## Take field from BSON. If field is missing and required than generate
+  ## "ReplyFieldMissing" exception. If field is missing and not required
+  ## than apply provided default value. If field exists than apply provided
+  ## calculations body code.
+  let b = reply[field]
+  if b == nil:
+    if isRequired:
+      raise newException(ReplyFieldMissing, "Required field \"" & field & "\" missing in reply")
+    else:
+      result = default
+  else:
+    body
+
+proc parseReplyOk(reply: Bson, isRequired: bool): bool {.raises: [ReplyFieldMissing, Exception]} =
+  ## Parse "ok" field in database reply.
+  parseReplyField(val, "ok", false):
+    result = val == 1.0'f64
+
+proc parseReplyN(reply: Bson, isRequired: bool): int {.raises: [ReplyFieldMissing, Exception]} =
+  ## Parse "n" field in database reply.
+  parseReplyField(val, "n", 0):
+    case val.kind
+    of BsonKindDouble:
+      result = val.toFloat64.int
+    else:
+      result = val.toInt
+
+proc parseReplyErrmsg(reply: Bson, isRequired: bool): string {.raises: [ReplyFieldMissing, Exception]} =
+  ## Parse "errmsg" field in database reply.
+  parseReplyField(val, "errmsg", ""):
+    if val.kind == BsonKindStringUTF8:
+      result = val
+    else:
+      result = ""
+
+proc toStatusReply*(reply: Bson): StatusReply =
+  ## Create StatusReply object from database reply BSON document.
+  ## "ok" field is considered required. "n" and "errmsg" fields
+  ## are optional and they are parsed if exist in reply
+  result.ok = parseReplyOk(reply, true)
+  result.n = parseReplyN(reply, false)
+  result.err = parseReplyErrmsg(reply, false)
+
+proc isReplyOk*(reply: Bson): bool =
+  ## This function is useful if we would like to check only "ok" field
+  ## in reply and do not like to create full StatusReply object. Field
+  ## is considered required
+  result = parseReplyOk(reply, true)
+
+proc getReplyN*(reply: Bson): int =
+  ## This function is useful if we would like to check only "n" field
+  ## in reply and do not like to create full StatusReply object. Field
+  ## is considered required
+  result = parseReplyN(reply, true)
+
+proc getReplyErrmsg*(reply: Bson): string =
+  ## This function is useful if we would like to check only "errmsg" field
+  ## in reply and do not like to create full StatusReply object. Field
+  ## is considered required
+  result = parseReplyErrmsg(reply, true)
 
 converter toBool*(sr: StatusReply): bool = sr.ok
   ## If StatusReply.ok field is true = then StatusReply is considered
